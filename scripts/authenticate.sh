1a9ac1c325da6ad679cf03c99ef5ebc8431d071f
Burrch3s <bryjeannotte@gmail.com>
2018-07-05 22:26:19 -0400

    small fixes

--- scripts/authenticate.sh
+++ scripts/authenticate.sh
@@ -4,6 +4,12 @@
 
 interface=$1
 
+if [[ $UID -gt 0 ]];
+then
+    echo "[X] Must run script as root!"
+    exit 1
+fi
+
 # if no arg passed, use default interface name
 if [[ $interface == '' ]];
 then
@@ -14,9 +20,9 @@ fi
 echo "[**] Turning off networking, the network manager"
 
 # Turn off network manager, networking and bring down interfaces
-sudo service network-manager stop
-sudo ifconfig wlp2s0b1 down
-sudo service networking stop
+service network-manager stop
+ifconfig $interface down
+service networking stop
 sleep 2
 
 # if no supplicant file, warn user and exit
@@ -27,4 +33,4 @@ then
 fi
 
 echo "[**] Connecting via wpa_supplicant with set configurations"
-sudo wpa_supplicant -Dnl80211 -i$interface -c/etc/wpa_supplicant.conf -B
+wpa_supplicant -Dnl80211 -i$interface -c/etc/wpa_supplicant.conf -B
